d3cc58ac1ec4538b08284bcf5225c05c70dc2414
Claudia Atkinson Bedia <115951660+ClaudiaAtkinsonBedia@users.noreply.github.com>
2022-11-03 22:11:47 +0100

    Añadidas cosas ejercicio 3

--- js/ejercicio03.js
+++ js/ejercicio03.js
@@ -1,8 +1,25 @@
 let caja = document.getElementById("info");
 let body = document.getElementById("cuerpo");
+let ratonTeclado = document.getElementById("rT");
+const pageX = document.getElementById("x");
+const pageY = document.getElementById("y");
+let teclaP = document.getElementById("teclaPulsada");
+let codTeclaP = document.getElementById("codigoTeclaPulsada");
 body.addEventListener("mousedown", cambiarColor);
 body.addEventListener("keypress", cambiarColor2);
 body.addEventListener("mousemove", volverABlanco);
+body.addEventListener("mousemove", funcionRaton);
+body.addEventListener("keypress", teclado);
+body.addEventListener("mousemove", updateDisplay, false);
+body.addEventListener("mouseenter", updateDisplay, false);
+body.addEventListener("mouseleave", updateDisplay, false);
+body.addEventListener("keypress", presionarTecla);
+
+function updateDisplay(event) 
+{
+    pageX.innerText = event.pageX;
+    pageY.innerText = event.pageY;
+}
 
 function cambiarColor()
 {
@@ -17,4 +34,22 @@ function cambiarColor2()
 function volverABlanco()
 {
     caja.style.backgroundColor = "white";
+}
+
+function funcionRaton()
+{
+    ratonTeclado.innerHTML = "Ratón";
+}
+
+function teclado()
+{
+    ratonTeclado.innerHTML = "Teclado";
+}
+
+function presionarTecla(e)
+{
+    let tecla = e.key;
+    let codTecla = e.keyCode;
+    teclaP.innerHTML = "Carácter[" + tecla + "]";
+    codTeclaP.innerHTML = "Código[" + codTecla + "]";
 }
